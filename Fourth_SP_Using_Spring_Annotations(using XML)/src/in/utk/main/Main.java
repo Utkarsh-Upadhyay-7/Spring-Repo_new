5c0e822ee71e536daee74f177c988377448de034
Utkarsh-Upadhyay-7 <123894031+Utkarsh-Upadhyay-7@users.noreply.github.com>
2025-10-12 19:41:27 +0530

    Add files via upload

---
+++ Fourth_SP_Using_Spring_Annotations(using XML)/src/in/utk/main/Main.java
@@ -0,0 +1,17 @@
+package in.utk.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.utk.beans.Student;
+
+public class Main { 
+	public static void main(String[] args) {
+		String resource_file_path = "/in/utk/resources/applicationContext.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(resource_file_path);
+		
+		Student std = (Student) context.getBean("student");
+		std.display();
+	}
+
+}

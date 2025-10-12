706f61fb8b956fb82aa9d775bc3f3b858b93c856
Utkarsh-Upadhyay-7 <123894031+Utkarsh-Upadhyay-7@users.noreply.github.com>
2025-10-12 18:25:37 +0530

    Add files via upload
    Spring Project using an Xml Configuration

---
+++ First_Spring_Project_Using_XML/src/in/Utkarsh/main/Main.java
@@ -0,0 +1,18 @@
+package in.Utkarsh.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.Utkarsh.beans.Student;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		String config_loc = "/in/Utkarsh/resources/applicationContext.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(config_loc);
+		
+		Student std = (Student) context.getBean("stdId");
+		std.display();
+	}
+
+}
